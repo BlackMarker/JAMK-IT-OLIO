d2680cca81678d43606fdd8036b1c1a1d317c321
K8811 <K8811@ICT-421-WS-26>
2017-01-11 14:57:29 +0200

    Muokattu BlackJack peli

--- BlackJackV0/BlackJackV0/BlackJack.cs
+++ BlackJackV0/BlackJackV0/BlackJack.cs
@@ -4,31 +4,52 @@
 /// </summary>
 namespace JAMK.IT
 {
-  class BlackJack
-  {
-    static void Main()
-    {
-      int myNumber = 17;
-      int theirNumber;
-      System.Console.WriteLine("*** BlackJack! ***");
-      System.Console.Write("Can you beat my number? Enter any number between 1-21: ");
-      //reading and converting 
-      theirNumber = System.Convert.ToInt32(System.Console.ReadLine());
-      //comparing that given umber is valid
-      if (theirNumber < 1 || theirNumber > 21)
-      {
-        Console.WriteLine("The given number is out of limits, try again.");
-      }
-      else {
-        //comparing
-        if (theirNumber >= myNumber && theirNumber <= 21)
-        {
-          System.Console.WriteLine("You win.");
-        }
-        else {
-          System.Console.WriteLine("You lose.");
-        }
-      }
-    }
-  }
+	class BlackJack
+	{
+		static void Main() {
+
+			while (true) {
+				//Arvotaan talon numero
+				Random Arvonta = new Random();
+				int myNumber = Arvonta.Next(10, 21);
+				//alustetaan pelaajan numero
+				int theirNumber;
+				System.Console.WriteLine("*** BlackJack! ***");
+				//Kysytään ja tallennetaan käyttäjältä luku muuttujaan "teksti"
+				System.Console.Write("Can you beat my number? Enter any number between 1-21: ");
+				string teksti = Console.ReadLine();
+				//jos teksti == x --> Suljetaan Ohjelma
+				if (teksti == "exit" || teksti == "x") {
+					Console.WriteLine("Suljit ohjelman");
+					Console.WriteLine();
+					break;
+				}
+				else {
+					//Jos ei suljeta ohjelmaa, tarkistetaan voidaanko muuttujaa kääntää int muotoon.
+					bool voiko = Int32.TryParse(teksti, out theirNumber);
+					if (voiko) {
+						theirNumber = System.Convert.ToInt32(teksti);
+						if (theirNumber > 0 || theirNumber < 22) {
+							if (theirNumber >= myNumber && theirNumber <= 21) {
+								System.Console.WriteLine("You win.");
+							}
+							else {
+								System.Console.WriteLine("You lose.");
+							}
+						}
+					}
+					else {
+						Console.WriteLine("Epakelpo arvo\n");
+					}
+					
+
+					
+					//if (theirNumber   {
+					//	Console.WriteLine("The given number is out of limits, try again.");
+					//}
+
+				}
+			}
+		}
+	}
 }
